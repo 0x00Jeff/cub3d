ed0fe8f8b68670a722544550c439ba0ae5d209b2
Abdelhamid Fatimi <jeff_septiceye@protonmail.com>
2024-01-21 16:49:07 +0100

    Feat: moving smoothly now using loop hook

--- include/hooks.h
+++ include/hooks.h
@@ -6,7 +6,7 @@
 /*   By: afatimi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/20 12:28:41 by afatimi           #+#    #+#             */
-/*   Updated: 2024/01/20 20:47:53 by afatimi          ###   ########.fr       */
+/*   Updated: 2024/01/21 16:43:25 by afatimi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,5 @@ enum
 };
 void	install_hooks(t_vars *vars);
 void	win_close(void *param);
-void	print_key(mlx_key_data_t keydata, void *param);
+void	print_key(void *param);
 #endif
--- src/hooks.c
+++ src/hooks.c
@@ -6,7 +6,7 @@
 /*   By: afatimi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/20 12:28:41 by afatimi           #+#    #+#             */
-/*   Updated: 2024/01/20 22:28:20 by afatimi          ###   ########.fr       */
+/*   Updated: 2024/01/21 16:46:44 by afatimi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 
 void	install_hooks(t_vars *vars)
 {
-	mlx_key_hook(vars->mlx, print_key, vars);
+	mlx_loop_hook(vars->mlx, print_key, vars);
 	mlx_close_hook(vars->mlx, win_close, vars->mlx);
 }
 
@@ -25,22 +25,25 @@ void	win_close(void *param)
 	mlx_close_window(param);
 }
 
-void	print_key(mlx_key_data_t keydata, void *param)
+void	print_key(void *param)
 {
 	static int	start_x;
 	static int	start_y;
 	int x_step;
 	int y_step;
 
-	x_step = 50;
-	y_step = 50;
-	printf("key pressed : %d\n", keydata.key);
-	if (keydata.key == MLX_KEY_ESCAPE)
+	x_step = 10;
+	y_step = 10;
+	if (mlx_is_key_down(((t_vars *)param) -> mlx, MLX_KEY_ESCAPE))
 		win_close(((t_vars *)param)->mlx);
 	clear_screen(param, start_x, start_y);
-	start_x += x_step * (keydata.key == MLX_KEY_RIGHT) * (start_x + x_step + 69 <= 1920) -
-		x_step * (keydata.key == MLX_KEY_LEFT) * (start_x > 0);
-	start_y += y_step * (keydata.key == MLX_KEY_DOWN) * (start_y + y_step + 69 <= 1080) -
-		y_step * (keydata.key == MLX_KEY_UP) * (start_y > 0);
+	if (mlx_is_key_down(((t_vars *)param) -> mlx, MLX_KEY_RIGHT))
+		start_x += x_step * (start_x + x_step + 69 <= 1920);
+	if (mlx_is_key_down(((t_vars *)param) -> mlx, MLX_KEY_LEFT))
+		start_x -= x_step * (start_x > 0);
+	if (mlx_is_key_down(((t_vars *)param) -> mlx, MLX_KEY_DOWN))
+		start_y += y_step * (start_y + y_step + 69 <= 1080);
+	if (mlx_is_key_down(((t_vars *)param) -> mlx, MLX_KEY_UP))
+		start_y -= y_step * (start_y > 0);
 	draw_shit(param, start_x, start_y);
 }
